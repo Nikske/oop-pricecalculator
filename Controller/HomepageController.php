72fee6ec82b0efdf4eeb4b2b4186ed22a6675476
Nikske <wolfvdd@gmail.com>
2020-02-17 10:57:57 +0100

    getting from json

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -5,7 +5,8 @@ ini_set('display_errors', "1");
 ini_set('display_startup_errors', "1");
 error_reporting(E_ALL);
 
-require_once 'Model/User.php';
+$json = file_get_contents('json/customers.json');
+$data = json_decode($json);
 
 class HomepageController
 {
