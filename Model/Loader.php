08d0d056090a6a331e22ba1cfece791d60b02531
SamiraBelkhiri <belkhirisamira@hotmail.com>
2020-02-19 14:51:51 +0100

    functions for loader made

--- Model/Loader.php
+++ Model/Loader.php
@@ -7,7 +7,16 @@ class Loader {
     private $productData;
     private $groupData;
 
-    public function loadCustomers() {
-        
+    public function loadCustomers() : array {
+        $this->customerData = json_decode(file_get_contents('json/customers.json'), true);
+        return $this->customerData;
+    }
+    public function loadProducts() : array {
+    $this->productData = json_decode(file_get_contents('json/products.json'), true);
+    return $this->productData;
+    }
+    public function loadGroups(): array {
+        $this->groupData = json_decode(file_get_contents('json/groups.json'), true);
+        return $this->groupData;
     }
 }
