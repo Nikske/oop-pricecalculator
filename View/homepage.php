a64e4074423445a6428a2408e00ba93af671ac61
Nikske <wolfvdd@gmail.com>
2020-02-20 15:05:08 +0100

    Calculate discount function

--- View/homepage.php
+++ View/homepage.php
@@ -36,7 +36,7 @@
         <p>Thank you <?php echo $_SESSION['customers'][$_POST['inputCustomers']]->getName()?> for placing your order </p>
 
         <p>Your chosen product is   <?php echo $_SESSION['products'][$_POST['inputProducts']]->getName()?> and will be shipped out as soon as possible </p>
-        <p>It will cost you a whole &#8364; <?php echo $_SESSION['products'][$_POST['inputProducts']]->getPrice() ?></p>
+        <p>The original price is: &#8364; <?php echo $_SESSION['products'][$_POST['inputProducts']]->getPrice() ?></p>
     </section>
     <?php require 'includes/footer.php'?>
 </body>
